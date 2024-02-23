cb93d850c313dcc135d58b3e791b7a92c44e1696
Atya22 <aytaj2002eva@gmail.com>
2024-02-23 23:46:03 +0400

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Prints a random number and states
+*whether it is positive, negative, or zero.
+*i
+*Return: always 0.
+**/
+
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{ putchar (letter);
+}
+putchar('\n');
+return (0);
+}
+
