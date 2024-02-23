b94a98b5f7102a8585835404ea820f8bc8217cc9
Atya22 <aytaj2002eva@gmail.com>
2024-02-24 00:17:39 +0400

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+* main - prints alphabet in lowercase,
+*and then in uppercase
+*
+* Return: always value(0).
+**/
+
+int main(void)
+{
+char upper_letter
+char lower_letter
+for (lower_letter = 'a'; lower_letter <= 'z'; lower_letter++)
+{
+putchar(lower_letter);
+}
+for (upper_letter = 'A'; upper_letter <= 'Z'; upper_letter++)
+{
+putchar(upper_letter);
+}
+putchar('\n')
+return (0)
+}
+
