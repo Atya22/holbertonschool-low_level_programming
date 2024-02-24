35a03a32f445a74416e7a21bc5692c8e8df2c57c
Atya22 <aytaj2002eva@gmail.com>
2024-02-24 16:09:01 +0400

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - function that prints all single
+*digit numbers of base 10 strarting from 0
+*
+*Return: always (0);
+**/
+
+int main(void)
+{char number;
+for (number = 1; number <= 10; number++)
+{
+putchar (number);
+}
+putchar ('\n');
+return (0);
+}
