daa69be85dc175d0ba6ad5ab8191afb67cade06a
Atya22 <aytaj2002eva@gmail.com>
2024-02-24 18:24:16 +0400

    Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+*main - function that prints the lowwercase
+*alphabet in reverse
+*
+*Return: always (0)
+**/
+
+int main(void)
+{
+char rev_letters
+for (rev_letters = 'z'; rev_letters >= 'a'; rev_letters--)
+{
+putchar(rev_letters);
+}
+putchar('\n');
+return (0);
+}
