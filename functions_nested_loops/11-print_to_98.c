c9123690ab996d1a3584ee9cb5babd8046ac839c
Atya22 <aytaj2002eva@gmail.com>
2024-02-24 23:22:25 +0400

    98 Battery Street, the OG

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 /**
 * print_to_98 - Prints all natural numbers from input to 98,
-*.
+*
 * @n: The number to begin counting at.
 */
-
 void print_to_98(int n)
 {
 for (; n < 98; n++)
@@ -16,5 +15,9 @@ for (; n > 98; n--)
 {
 printf("%d, ", n);
 }
-printf("%d, ", n);
+printf("%d\n", n);
 }
+
+
+
+
