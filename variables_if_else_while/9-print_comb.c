86f76deb0fad8ce6e68d4ab41141e3c00a2da194
Atya22 <aytaj2002eva@gmail.com>
2024-02-24 19:50:52 +0400

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+*main - function that prints all the numbers
+*of base 16 in lowercase
+*
+*Return: Always (0);
+**/
+
+int main(void)
+{
+int number;
+for (number = '0'; number <= '9'; number++)
+{
+putchar(number);
+if ((number >= '0') && (number < '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
