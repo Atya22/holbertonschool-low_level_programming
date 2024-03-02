9682533c8f8ca9eaf442cc09c5d6898b73de5981
Atya22 <aytaj2002eva@gmail.com>
2024-03-03 00:03:47 +0400

    I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+nclude"main.h"
+
+/**
+* reverse_array - Make a reverse array
+* @a: first array
+* @n: second array
+*/
+
+void reverse_array(int *a, int n)
+{
+		int i, tmp;
+
+		for (i = 0; i < n / 2; i++)
+		{
+			tmp = a[i];
+			a[i] = a[n - i - 1];
+			a[n - i - 1] = tmp;
+		}
+}
