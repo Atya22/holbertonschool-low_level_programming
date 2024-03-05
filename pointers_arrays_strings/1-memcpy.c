1414a4124d1b4cdd081e0981ce21a1029c5f5d6b
Atya22 <aytaj2002eva@gmail.com>
2024-03-05 18:10:43 +0400

    memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
 *char *_memcpy - function
 *@dest:var
@@ -9,11 +9,12 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src;
+		dest[i] = src[i];
 	}
+	return (dest);
 }
 
