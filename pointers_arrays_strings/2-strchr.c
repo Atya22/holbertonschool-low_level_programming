c1139f539413fd2da44c2fd15a49e48e2d5bb9e8
Atya22 <aytaj2002eva@gmail.com>
2024-03-05 19:09:03 +0400

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,11 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for(i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 
 		if (s[i] == c)
-			return (s+i);
+			return (s + i);
 	}
 	return (NULL);
 }
