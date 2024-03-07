d5b38d69a8ee2c7814992ce9aead0af53e01db5f
Atya22 <aytaj2002eva@gmail.com>
2024-03-08 01:22:02 +0400

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-*_strstr - function that that locates a substring
+*_strstr - function that searches a string for any of a set of bytes
 *@haystack: holds value
 *@needle: holds value
 *Return: Always 0.
@@ -9,16 +10,36 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	int n_length;
 	int i;
 	int j;
+	int found = 0;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	for (n_length = 0; needle[n_length] != '\0'; n_length++)
+		;
+
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[i] == haystack[j])
+			if (haystack[i] == '\0')
 			{
-				return (haystack + j);
+				return (NULL);
+			}
+			else if (needle[j] == haystack[i])
+			{
+				i++;
+				found++;
+
+				if (found == n_length)
+				{
+					return (needle + (i - n_length));
+				}
+			}
+			else
+			{
+				found = 0;
+				break;
 			}
 		}
 	}
