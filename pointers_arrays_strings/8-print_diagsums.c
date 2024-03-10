fb824b6f08665ca8f2d7bfac3594cacabb69ad49
Atya22 <aytaj2002eva@gmail.com>
2024-03-10 07:55:38 +0400

    chessboard diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -13,14 +13,14 @@ void print_diagsums(int *a, int size)
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
 		sum1 = sum1 + a[i * size + i];
 	}
 
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
-		sum2 = sum2 + a[i * (size - 1)];
+		sum2 = sum2 + a[i * size + (size - 1 - i)];
 	}
 
 	printf("%d, %d\n", sum1, sum2);
