542be690c9285dad8ce631756a524dca4c47812d
Atya22 <aytaj2002eva@gmail.com>
2024-03-10 23:19:01 +0400

    puts recursion, rev recursion, strlen recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+* _puts_recursion -entry point,prints string
+* @s: -holds value for output
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
