16b9a426adf01f287875a66e2dcf07dfd16ec559
Atya22 <aytaj2002eva@gmail.com>
2024-03-10 23:33:45 +0400

    factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+* factorial -entry point,return factorial of number
+* @n: -holds value for output
+* Return: (i * factorial(n - 1))
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+		return (n * factorial(n - 1));
+}
