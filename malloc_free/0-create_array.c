541fc3ee7db17b1bde2c748ee135325aedb719ac
Atya22 <aytaj2002eva@gmail.com>
2024-03-16 17:13:19 +0400

    create array function

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,7 +3,33 @@
 /**
 *create_array - function that creates an array of chars, and
 *initializes it with a specific char
-*@size
-*@c
+*@size: size of array
+*@c: char
+*
+*Return: always 0.
 */
+
+char* create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char* buff;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	buff = (char*) malloc(size * (sizeof(char)));
+	if (buff == NULL)
+	{
+		return (NULL);
+	}
+
+	for(i = 0; i < size; i++)
+	{
+		buff[i] = c;
+	}
+
+	return (buff);
+}
 
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
