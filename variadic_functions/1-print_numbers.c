c41679427693605e50ccb26226298fbecb14217d
Atya22 <aytaj2002eva@gmail.com>
2024-03-24 23:27:47 +0400

    1 task and prototypes

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include<stdarg.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+* print_numbers -entry point,prints numbers
+* @separator: -points to the respective char
+* @n: -holds value for output
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ptr;
+
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ptr, int));
+
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
