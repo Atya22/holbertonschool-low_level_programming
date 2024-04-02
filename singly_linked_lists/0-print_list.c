1e8410d99517929baa073b80c6180c5d9ec5dac5
Atya22 <aytaj2002eva@gmail.com>
2024-04-02 16:48:14 +0400

    Print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,1 +1,28 @@
-#include <main.h>
+#include "lists.h"
+
+/**
+*print_list - function with one argument
+*@h: const pointer type;
+*Return: (count);
+*/
+
+size_t print_list(count list_t *h)
+{
+	int count = 0;
+
+	while (h != 0)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("%[u] %s", h->len, h->str);
+
+			h = h->next
+			count++
+		}
+	return (count);
+	}
+}
