de287672688c3d9c4b37c3aafb718f693e183678
Atya22 <aytaj2002eva@gmail.com>
2024-04-02 17:19:34 +0400

    Print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -19,10 +19,10 @@ size_t print_list(const list_t *h)
 		else
 		{
 			printf("[%u] %s\n", h->len, h->str);
-
-			h = h->next;
-			count++;
 		}
+
+		h = h->next;
+		count++;
 	}
 
 	return (count);
