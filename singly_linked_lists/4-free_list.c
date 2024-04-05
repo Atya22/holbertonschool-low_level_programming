fb2181a92c6ffe09c25b208fd6b65eb85aa36b61
Atya22 <aytaj2002eva@gmail.com>
2024-04-05 18:54:58 +0400

    2,3,4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include"lists.h"
+
+/**
+* free_list -frees the sll
+* @head: -points to the first node
+*/
+
+void free_list(list_t *head)
+{
+	list_t *helper;
+
+	while (head != NULL)
+	{
+		helper = head;
+		head = head->next;
+		free(helper->str);
+		free(helper);
+	}
+}
