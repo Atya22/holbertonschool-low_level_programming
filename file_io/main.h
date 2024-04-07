8e316fc4eff10ad8cbc9d3b4a1d1271fe6f77fa8
Atya22 <aytaj2002eva@gmail.com>
2024-04-07 23:19:36 +0400

    added libraries

--- file_io/main.h
+++ file_io/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
