04e856fda96edb4c140bc7d45bca17bebad473bd
Atya22 <aytaj2002eva@gmail.com>
2024-04-07 19:57:45 +0400

    11

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,11 +1,11 @@
 #include"main.h"
 
 /**
- * get_bit - gets git value at specific index
- * @index: -holds value for output
- * @n: -holds value for output
- * Return: (a >> index & 1)
- */
+* set_bit - gets git value at specific index
+* @index: -holds value for output
+* @n: -holds value for output
+* Return: (a >> index & 1)
+*/
 
 int set_bit(unsigned long int *n, unsigned int index)
 
